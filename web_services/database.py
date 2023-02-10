ae3fbd64376a2eab31a5a51c7d3bd0cdcd379eb4
iamtushar <ecetushar22@gmail.com>
2023-02-11 02:10:23 +0530

    adds device self registering feature on first boot of device

--- web_services/database.py
+++ web_services/database.py
@@ -1,11 +1,11 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 eng = create_engine("sqlite:///web_services/dockManagement.db", echo=True)
 
 Base = declarative_base()
 
-def init_db(): 
-    Base.metadata.create_all(eng)
-    return
+
+Session = sessionmaker(bind=eng, autoflush=True)
